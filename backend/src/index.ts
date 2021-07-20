03fe4775985f46207889ef6bd95d450ca9fe827c
colxi Sergi Guzman <sergi.guzman@glovoapp.com>
2021-07-21 00:36:07 +0200

    cleanup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,26 +2,16 @@ import ErrorHandlingService from './service/error-handling'
 import BinanceService from './service/binance-service'
 import { config } from './config'
 
-// import binanceClient from './service/futures-account'
 // import initRESTApiService from './service/api-server'
-// import futuresWalletService from './service/futures-wallet'
-// import indicatorsFeed from './service/indicators-feed'
 
 // make Binance object available for debugging pùrposes (yarn debug + devtools)
 (global as any).Binance = BinanceService
 
 console.clear()
 
-function validate(): void {
-  const activeCoinsCount = Object.values(config.assets).filter(i => i.enabled).length
-  console.log('ENABLED COINS :', activeCoinsCount)
-  if (activeCoinsCount > 199) throw new Error('a maximum of 199 coins are allowed simultanously')
-}
-
 async function init(): Promise<void> {
   // TODO:  Get all available symbols, set leverage to X5 to all symbols, set all  symbols to isolated.
   await ErrorHandlingService.start()
-  validate()
   await BinanceService.start({
     assetPairs: config.getEnabledAssetsList()
   })
